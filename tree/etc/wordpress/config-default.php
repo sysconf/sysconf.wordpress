68d0580278c6e854127da890327bc865c00d16a9
JF Gigand <jf@geonef.fr>
2017-08-31 19:47:11 +0200

    sysconf.wordpress

---
+++ tree/etc/wordpress/config-default.php
@@ -0,0 +1,9 @@
+<?php
+# Provided by sysconf.wordpress
+
+define('DB_NAME', 'wp0');
+define('DB_USER', 'wp0');
+define('DB_PASSWORD', 'soo1EiHeAhf1aiti');
+define('DB_HOST', 'localhost');
+define('SECRET_KEY', 'manu1eeGuV2ooShiOhf0ku7ZAhl8ik7EZiapa0ah');
+define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
