6aaa00c41469308fac77f38d808361e7cf97a97c
JF Gigand <jf@geonef.fr>
2017-10-29 12:54:27 +0000

    Gitted driver-git-remote auto-commit from root@rers.gigand.adhara:/sysconf

---
+++ tree/etc/wordpress/config-default.d/ssh-auth.php
@@ -0,0 +1,7 @@
+<?php
+
+define('FTP_PUBKEY','/home/wp-user/id_rsa.pub');
+define('FTP_PRIKEY','/home/wp-user/id_rsa');
+define('FTP_USER','wp-user');
+define('FTP_PASS','');
+define('FTP_HOST','127.0.0.1:22');
--- tree/etc/wordpress/config-default.php
+++ tree/etc/wordpress/config-default.php
@@ -7,3 +7,14 @@ define('DB_PASSWORD', 'soo1EiHeAhf1aiti');
 define('DB_HOST', 'localhost');
 define('SECRET_KEY', 'manu1eeGuV2ooShiOhf0ku7ZAhl8ik7EZiapa0ah');
 define('WP_CONTENT_DIR', '/var/lib/wordpress/wp-content');
+
+
+/* die(__FILE__."++"); */
+$_config_dir = dirname(__FILE__).'/'.basename(__FILE__, '.php').".d";
+/* die($_config_dir); */
+if (is_dir($_config_dir)) {
+    foreach (glob($_config_dir."/*.php") as $filename)
+        {
+            include $filename;
+        }
+}
